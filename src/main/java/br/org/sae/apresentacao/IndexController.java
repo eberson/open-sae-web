61ae5d768e23bb5746f1ccc956c495299276b9a7
aluno <aluno@L02M02>
2013-12-16 23:31:37 -0200

    Ajustes para testes com o Jetty

--- src/main/java/br/org/sae/apresentacao/IndexController.java
+++ src/main/java/br/org/sae/apresentacao/IndexController.java
@@ -1,6 +1,7 @@
 package br.org.sae.apresentacao;
 
 import br.com.caelum.vraptor.Get;
+import br.com.caelum.vraptor.Path;
 import br.com.caelum.vraptor.Resource;
 import br.com.caelum.vraptor.Result;
 
@@ -8,9 +9,10 @@ import br.com.caelum.vraptor.Result;
 public class IndexController {
 
 	@Get
+	@Path("/")
 	public void index(Result resultado) {
 
-		resultado.include("mensagem", "deu certo");
+		resultado.include("mensagem", "mudando essa *");
 
 	}
 
