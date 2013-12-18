e977e3b865c29393133dcd39ad15e2abb466a479
SySNetwork Soluções <sysnetwork@sysnetwork>
2013-12-17 22:39:10 -0200

    Incluido um TestSuite para o projeto

---
+++ src/test/java/br/org/sae/apresentacao/ApresentacaoTestSuite.java
@@ -0,0 +1,11 @@
+package br.org.sae.apresentacao;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ ImportarPlanilhaControllerTest.class, IndexControllerTest.class })
+public class ApresentacaoTestSuite {
+ // Nada
+}
