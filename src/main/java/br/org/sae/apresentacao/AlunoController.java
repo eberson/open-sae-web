475c90f76281f464bde2359e1cbe010352a7e3ad
Lázaro <lazarodm@gmail.com>
2013-12-24 02:33:35 -0200

    Lista de Aluno para o método AlunoController.listar, que retornava uma lista genérica

--- src/main/java/br/org/sae/apresentacao/AlunoController.java
+++ src/main/java/br/org/sae/apresentacao/AlunoController.java
@@ -5,13 +5,14 @@ import java.util.List;
 import br.com.caelum.vraptor.Get;
 import br.com.caelum.vraptor.Path;
 import br.com.caelum.vraptor.Resource;
+import br.org.sae.model.Aluno;
 
 @Resource
 public class AlunoController {
 
 	@Get
 	@Path("/aluno/consultar")
-	public List consultar() {
+	public List<Aluno> consultar() {
 		return null;
 	}
 
